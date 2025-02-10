e81888f6bfed5e842564d613106541faf17afef3
seleniumforchitta <seleniumforchitta@gmail.com>
2025-02-10 14:39:12 +0530

    PR for Built-in metrics

--- Assertions/k6-threshold-configuration.js
+++ Assertions/k6-threshold-configuration.js
@@ -6,8 +6,11 @@ export const options = {
     vus : 10,
     duration: '10s',
     thresholds: {
-        http_req_duration: ['p(95)<100'],
-        http_req_failed: ['rate<0.01']
+        http_req_duration: ['p(95)<500'],
+        http_req_failed: ['rate<0.01'],
+        http_reqs: ['rate>4'], // we have 2 metrice available count & rate
+        // From the O/P -  http_reqs......................: 329     32.226885/s - Means total 329 request * rate is 32 request per second
+        vus: ['value>9']
     }
 }
 
